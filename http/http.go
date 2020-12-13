d946761bcb2064bfc6e0beb9734dfc2d3c95918c
alexhokl <alexhokl@gmail.com>
2020-12-13 14:47:24 +0800

    Added more functions to package http

--- http/http.go
+++ http/http.go
@@ -2,11 +2,22 @@ package http
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 // SetBearerTokenHeader sets the Authorization header with the specified bearer
 // token
 func SetBearerTokenHeader(req *http.Request, token string) {
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+}
+
+// DumpResponse dumps the body of the specified response to stdout
+func DumpResponse(resp *http.Response) error {
+	_, err := io.Copy(os.Stdout, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
 }
