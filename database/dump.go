fbc40b38d421eb64004df212523880780798950e
alexhokl <alexhokl@gmail.com>
2021-01-22 22:21:36 +0800

    Added more functions to package database

--- database/dump.go
+++ database/dump.go
@@ -10,15 +10,24 @@ import (
 
 // DumpTables prints the specified tables to standard output stream
 func DumpTables(list []TableData) error {
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', tabwriter.AlignRight)
 	for _, data := range list {
-		fmt.Fprintf(w, "%s\t\n", strings.Join(data.Columns, "\t"))
-		for _, r := range data.Rows {
-			vals := getStringValues(r)
-			fmt.Fprintf(w, "%s\t\n", strings.Join(vals, "\t"))
+		err := DumpTable(&data)
+		if err != nil {
+			return err
 		}
-		w.Flush()
 	}
+	return nil
+}
+
+// DumpTable prints the specified table to standard output stream
+func DumpTable(data *TableData) error {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', tabwriter.AlignRight)
+	fmt.Fprintf(w, "%s\t\n", strings.Join(data.Columns, "\t"))
+	for _, r := range data.Rows {
+		vals := getStringValues(r)
+		fmt.Fprintf(w, "%s\t\n", strings.Join(vals, "\t"))
+	}
+	w.Flush()
 	return nil
 }
 
