b08a30b06d7af186a5beef8639003f4de04254da
alexhokl <alexhokl@gmail.com>
2022-05-18 20:51:41 +0800

    Refactored googleapi

--- googleapi/auth.go
+++ googleapi/auth.go
@@ -2,19 +2,13 @@ package googleapi
 
 import (
 	"context"
-	"crypto/tls"
-	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/url"
-	"os/exec"
 	"strings"
-	"sync"
-	"time"
 
 	"googlemaps.github.io/maps"
 
-	"github.com/alexhokl/helper/cli"
+	"github.com/alexhokl/helper/authhelper"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
@@ -50,9 +44,22 @@ func New(ctx context.Context, googleClientSecretFilePath string, accessToken str
 }
 
 func (client *GoogleClient) GetToken() (*oauth2.Token, error) {
-	client.Config.RedirectURL = fmt.Sprintf("http://localhost:%d%s", port, callbackUri)
+	token, err := authhelper.GetToken(
+		client.Context,
+		authhelper.OAuthConfig{
+			ClientId: client.Config.ClientID,
+			ClientSecret: client.Config.ClientSecret,
+			Scopes: client.Config.Scopes,
+			RedirectURI: client.Config.RedirectURL,
+			Port: port,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	client.Token = token
 
-	return getTokenFromBrowser(client, port)
+	return token, nil
 }
 
 func (client *GoogleClient) NewHttpClient() (*http.Client) {
@@ -61,83 +68,4 @@ func (client *GoogleClient) NewHttpClient() (*http.Client) {
 
 func NewMapClient(apiKey string) (*maps.Client, error) {
 	return maps.NewClient(maps.WithAPIKey(apiKey))
-}
-
-func getTokenFromBrowser(client *GoogleClient, port int) (*oauth2.Token, error) {
-	// add transport for self-signed certificate to context
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	sslcli := &http.Client{Transport: tr}
-	client.Context = context.WithValue(client.Context, oauth2.HTTPClient, sslcli)
-
-	url := client.Config.AuthCodeURL("state", oauth2.AccessTypeOffline)
-
-	fmt.Println("You will now be taken to your browser for authentication")
-	time.Sleep(1 * time.Second)
-	cmdName, cmdArgs := cli.GetOpenCommand(url)
-	_, errOpen := exec.Command(cmdName, cmdArgs...).Output()
-	if errOpen != nil {
-		return nil, errOpen
-	}
-	time.Sleep(1 * time.Second)
-
-	var token oauth2.Token
-	var err error
-	tokenDone := &sync.WaitGroup{}
-	tokenChannel := make(chan oauth2.Token, 1)
-	errorChannel := make(chan error, 1)
-	serverErrorChannel := make(chan error, 1)
-	callbackHandler := getTokenHandler(client, tokenChannel, errorChannel)
-	http.HandleFunc(callbackUri, callbackHandler)
-	server := getServer(port)
-	go func(tokens chan oauth2.Token) {
-		for t := range tokens {
-			token = t
-			tokenDone.Done()
-			return
-		}
-	}(tokenChannel)
-	go func(errs chan error) {
-		for e := range errs {
-			err = e
-			tokenDone.Done()
-			return
-		}
-	}(errorChannel)
-	tokenDone.Add(1)
-	go func() {
-		if err := server.ListenAndServe(); err != http.ErrServerClosed {
-			serverErrorChannel <- err
-		}
-		tokenDone.Done()
-	}()
-	tokenDone.Wait()
-
-	client.Token = &token
-
-	return &token, err
-}
-
-func getTokenHandler(client *GoogleClient, tokenChannel chan oauth2.Token, errorChannel chan error) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		queryParts, _ := url.ParseQuery(r.URL.RawQuery)
-		code := queryParts["code"][0]
-
-		token, err := client.Config.Exchange(client.Context, code)
-		if err != nil {
-			errorChannel <- err
-			return
-		}
-
-		tokenChannel <- *token
-		fmt.Fprintf(w, "You have been authenticated. This browser window can be closed.")
-	}
-}
-
-func getServer(port int) *http.Server {
-	server := &http.Server{
-		Addr: fmt.Sprintf(":%d", port),
-	}
-	return server
 }
