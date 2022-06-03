4f4dd3e1ba3bb4ab194d620a01894f8f6346f5cd
alexhokl <alexhokl@gmail.com>
2022-06-03 20:42:11 +0800

    Added helper function for viper in package cli

--- cli/cli.go
+++ cli/cli.go
@@ -2,9 +2,12 @@ package cli
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"runtime"
 	"strings"
+
+	"github.com/spf13/viper"
 )
 
 // GetOpenCommand returns the command and its required arguments according to
@@ -26,9 +29,37 @@ func OpenInBrowser(url string) error {
 	cmdName, cmdArgs := GetOpenCommand(url)
 	_, errOpen := exec.Command(cmdName, cmdArgs...).Output()
 	if errOpen != nil {
-		cmdParts := []string { cmdName }
+		cmdParts := []string{cmdName}
 		cmdParts = append(cmdParts, cmdArgs...)
 		return fmt.Errorf("unable to complete command [%s] %w", strings.Join(cmdParts, " "), errOpen)
 	}
 	return nil
+}
+
+func ConfigureViper(configFilePath string, applicationName string, verbose bool, environmentVariablePrefix string) {
+	if environmentVariablePrefix == "" {
+		environmentVariablePrefix = strings.ReplaceAll(applicationName, "-", "_")
+	}
+
+	if configFilePath != "" {
+		viper.SetConfigFile(configFilePath)
+	} else {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName(fmt.Sprintf(".%s", applicationName))
+	}
+
+	viper.SetEnvPrefix(environmentVariablePrefix)
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		if verbose {
+			fmt.Println("Using config file:", viper.ConfigFileUsed())
+		}
+	}
 }
