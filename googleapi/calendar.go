ec4fdbe2e58883829a4785495cbe5f9fcfed5e41
alexhokl <alexhokl@gmail.com>
2022-10-22 06:04:25 -0400

    Added calendar API to googleapis

---
+++ googleapi/calendar.go
@@ -0,0 +1,53 @@
+package googleapi
+
+import (
+	"context"
+	"fmt"
+
+	"golang.org/x/oauth2"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+)
+
+func NewCalendarService(ctx context.Context, oauthConfig oauth2.Config, token oauth2.Token) (*calendar.Service, error) {
+	httpClient := oauthConfig.Client(ctx, &token)
+	srv, err := calendar.NewService(ctx, option.WithHTTPClient(httpClient))
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve Calendar client: %v", err)
+	}
+	return srv, nil
+}
+
+func GetEvents(srv *calendar.Service, calendarName string, startTime string, endTime string, limit int64, orderBy string) ([]*calendar.Event, error) {
+	if calendarName == "" {
+		return nil, fmt.Errorf("calendar name is required")
+	}
+
+	call := srv.Events.List(calendarName)
+
+	if startTime != "" {
+		call = call.TimeMin(startTime)
+	}
+
+	if endTime != "" {
+		call = call.TimeMax(endTime)
+	}
+
+	if limit != 0 {
+		call = call.MaxResults(limit)
+	}
+
+	if orderBy != "" {
+		call = call.OrderBy(orderBy)
+	}
+
+	events, err := call.Do()
+	if err != nil {
+		return nil, err
+	}
+	if events.NextPageToken == "" {
+		return events.Items, nil
+	}
+
+	return events.Items, nil
+}
