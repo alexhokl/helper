0e9d9b3c464002ca1f7a43ec54b9969abc0d949d
alexhokl <alexhokl@gmail.com>
2023-04-04 23:10:55 +0800

    Updated iohelper

--- iohelper/file.go
+++ iohelper/file.go
@@ -1,6 +1,7 @@
 package iohelper
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -8,18 +9,48 @@ import (
 
 // ReadStringFromFile returns content of the file in the specified path as string
 func ReadStringFromFile(path string) (string, error) {
+	bytes, err := ReadBytesFromFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+// ReadBytesFromFile returns content of the file in the specified path as bytes
+func ReadBytesFromFile(path string) ([]byte, error) {
 	if path == "" {
-		return "", fmt.Errorf("path cannot be empty")
+		return nil, fmt.Errorf("path cannot be empty")
 	}
 	if !IsFileExist(path) {
-		return "", fmt.Errorf("File [%s] does not exist", path)
+		return nil, fmt.Errorf("file [%s] does not exist", path)
 	}
 	file, errFile := ioutil.ReadFile(path)
 	if errFile != nil {
-		return "", errFile
+		return nil, errFile
 	}
-	return string(file), nil
+	return file, nil
 }
+
+func ReadFirstLineBytesFromFile(path string) ([]byte, error) {
+	if path == "" {
+		return nil, fmt.Errorf("path cannot be empty")
+	}
+	if !IsFileExist(path) {
+		return nil, fmt.Errorf("file [%s] does not exist", path)
+	}
+	file, errFile := os.Open(path)
+	if errFile != nil {
+		return nil, errFile
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	if scanner.Scan() {
+		return scanner.Bytes(), nil
+	}
+	return nil, nil
+}
+
 
 // IsFileExist return true if a file exist in the specified path
 func IsFileExist(path string) bool {
