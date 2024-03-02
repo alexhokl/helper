23fb089135912f491ee421f864bcbe3f72ebe422
alexhokl <alexhokl@gmail.com>
2024-03-02 18:02:52 +0800

    Fixed a bug in regexhelper

--- regexhelper/regex.go
+++ regexhelper/regex.go
@@ -1,11 +1,16 @@
 package regexhelper
 
-import "regexp"
+import (
+	"regexp"
+)
 
 // FindNamedGroupMatchedStrings returns a map with named groups specified in the
 // specified regex
 func FindNamedGroupMatchedStrings(regex *regexp.Regexp, input string) map[string]string {
 	match := regex.FindStringSubmatch(input)
+	if len(match) == 0 {
+		return map[string]string{}
+	}
 	subMatchMap := make(map[string]string)
 	for i, name := range regex.SubexpNames() {
 		if i != 0 {
